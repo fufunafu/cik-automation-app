84359aa52698423ae972fcb22c866fa31a863409
Fuanne Gao <76637709+fufunafu@users.noreply.github.com>
2025-06-10 14:20:09 -0400

    Use PORT from environment

--- app.py
+++ app.py
@@ -223,4 +223,5 @@ def run_automation():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
